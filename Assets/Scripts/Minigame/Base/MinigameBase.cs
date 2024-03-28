05aa77d4d9f1551ea927a005dec282fdba4091fa
masaru0310 <126130688+masaru0310@users.noreply.github.com>
2024-03-28 15:05:10 +0900

    Feature/masaru0310/AddRightDirection
    ・ミニゲーム発生エリアに、正面から衝突したときのみ、ミニゲームを発生するようにした。

--- Assets/Scripts/Minigame/Base/MinigameBase.cs
+++ Assets/Scripts/Minigame/Base/MinigameBase.cs
@@ -21,34 +21,63 @@ public class MinigameBase : MonoBehaviour
     }
 
 
-
-
     /// <summary>
     /// ミニゲーム発生エリアに触れた後の処理
+    /// 注意!! ④つの条件を満たすと、ミニゲームが開始される
     /// </summary>
     /// <param name="other"></param>
-    protected void OnTriggerEnter(Collider other)
+    protected void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("衝突");
-
         // ① 触れたのが、プレイヤー以外だったら処理を終了
-        if (other.gameObject.tag != "Player") return;
-        Debug.Log("触れたのがバイク");
+        if (collision.gameObject.tag != "Player") return;
 
-        // ② ミニゲームの開始条件を達成していなかったら終了
+        // ② 各ミニゲームごとの開始条件を達成していなかったら終了
+        // ※ 注意!! 条件は、継承先で定義される
         if (IsJudgeStart() == false) return;
+
+        // ③ ミニゲーム発生するための方向に進行しているか
+        if (IsJudgeInRightDirection(collision) == false) return;
 
         // ミニゲーム開始を決める乱数を出力
         int randomNum = Random.Range(1, 100);
 
-        Debug.Log($"乱数 : {randomNum}");
-
         // ※ ここでプレイヤーの順位を確認し、順位によって確率を変更させる
 
-        // ③ 発生させる確率を引き当てたら、不運ミニゲーム開始
+        // ④ 発生させる確率を引き当てたら、継承先で不運ミニゲーム開始
         if (randomNum <= incidenceNum) isStartMinigame = true;
     }
 
+
+    private bool IsJudgeInRightDirection(Collision collision)
+    {
+        // 大まかな衝突位置を検出
+        Vector3 hitPos = collision.contacts[0].point;
+
+        // キャラクター正面から見た衝突位置の角度を取得
+        Vector3 diff = hitPos - transform.position;
+        Vector3 axis = Vector3.Cross(transform.forward, diff);
+        float angle = Vector3.Angle(transform.forward, diff) * (axis.y < 0 ? -1 : 1);
+
+        Debug.Log($"角度 : {angle}");
+
+        if ((angle <= 145.0f && angle >= 128.0f) || (angle >= -145.0f && angle <= -128.0f))
+        {
+            //// コリジョンから見て、プレイヤーがまっすぐ進んでいるかを確認
+            //if (Mathf.Abs(angle) <= 90.0f)
+            //{
+            //    return true;
+            //}
+            //// 別の角度から衝突した場合は、ミニゲーム発生条件を満たさない
+            //else return false;
+            Debug.Log("正面から突進");
+            return true;
+        }
+        else
+        {
+            Debug.Log("正面から突進していない");
+            return false;
+        }
+    }
 
     /// <summary>
     /// 触れた時と追加での発生条件
